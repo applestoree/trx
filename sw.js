027240f0dd85c35fbf18e9bd5baaffafdffba6ce
applestoree <applestoremalaysiaa@gmail.com>
2026-09-17 23:34:33 +0800

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'apple-store-malaysia-v1'
+
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()))
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request
+  if (request.method !== 'GET') return
+  const url = new URL(request.url)
+  if (url.origin !== self.location.origin) return
+  if (url.pathname.includes('/functions/')) return
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+      const copy = response.clone()
+      caches.open(CACHE).then((cache) => cache.put(request, copy))
+      return response
+    }).catch(() => caches.match('./')))
+  )
+})
